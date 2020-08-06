077fcd38108d22c5e9788ee518f62cabc9460ef9
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-08-06 08:50:09 -0500

    Select Language

---
+++ 05/select_language.py
@@ -0,0 +1,40 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+class LanguageOptions(unittest.TestCase):
+
+  def setUp(self):
+    driver = self.driver = webdriver.Chrome(executable_path = r'./chromedriver.exe')
+    driver.implicitly_wait(30)
+    driver.maximize_window()
+    driver.get('http://demo-store.seleniumacademy.com')
+
+  def test_select_language(self):
+    exp_options = ['English', 'French', 'German']
+    act_options = []
+
+    select_language = Select(self.driver.find_element_by_id('select-language'))
+
+    self.assertEqual(3, len(select_language.options))
+
+    for options in select_language.options:
+      act_options.append(options.text)
+      
+    self.assertEqual(exp_options, act_options)
+
+    self.assertEqual('English', select_language.first_selected_option.text)
+
+    select_language.select_by_visible_text('German')
+    self.assertTrue('store=german' in self.driver.current_url) # Esta es una forma de validarlo, pues se cambia en la URL, pero puede haber mas formas
+
+    select_language = Select(self.driver.find_element_by_id('select-language')) # Hay que reiniciar el objeto
+
+    select_language.select_by_index(0)
+
+  def tearDown(self):
+    self.driver.implicitly_wait(3)
+    self.driver.close()
+
+if __name__ == '__main__':
+  unittest.main(verbosity=2)
